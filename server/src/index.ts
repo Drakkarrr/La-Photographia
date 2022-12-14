28aed4c942e40b995778a123fc511e81a8cd0695
Junrey Lañas <jrydev404@gmail.com>
2022-12-15 04:31:43 +0800

    Fixed server issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 /* MIDDLEWARE CONFIGS */
 const app = express();
@@ -17,49 +16,47 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true } as bodyParser.OptionsJson));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true } as any));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
+app.use('/assets', express.static(path.join(__dirname, 'assets/saved')));
 
 /* FILE STORAGE */
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+const storage: multer.StorageEngine = multer.diskStorage({
+  destination: (req: express.Request, file: Express.Multer.File, cb: any) => {
+    cb(null, 'assets/saved');
+  },
+  filename: (req: express.Request, file: Express.Multer.File, cb: any) => {
+    cb(null, file.originalname);
+  },
 });
-const upload = multer({ storage });
-
+const upload: multer.Multer = multer({ storage });
 
 /* ROUTES WITH FILES */
 // app.post("/auth/register", upload.single("picture"), register);
 // app.post("/posts", verifyToken, upload.single("picture"), createPost);
-
 
 /* ROUTES */
 // app.use("/auth", authRoutes);
 // app.use("/users", userRoutes);
 // app.use("/posts", postRoutes);
 
-
 /* MONGOOSE CONFIG */
-// const PORT = process.env.PORT || 6001;
-// mongoose
-//     .connect(process.env.MONGO_URL, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-    // })
-    // .then(() => {
-        // app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+const PORT: string | number = process.env.PORT || 8080;
+mongoose
+  .set("strictQuery", false)
+  .connect(process.env.MONGO_URL as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running at Port: ${PORT}`));
 
-/* ADD DATA ONE TIME */
-    //     User.insertMany(users);
-    //     Post.insertMany(posts);
-    // })
-    // .catch((error) => console.log(`${error} did not connect`));
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} Failed to connect to Server`));
+
